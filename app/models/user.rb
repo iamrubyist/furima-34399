f49a68f7e52ada76eeab403136abfd39ded229bc
iamrubyist <takagioffcial4@gmail.com>
2021-02-28 19:43:16 +0900

    with_options
    what
    with_optionsを使ってコードをスッキリさせた
    why
    見やすくするため

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name,          presence: :true
-  validates :first_name,    presence: :true
-  validates :last_name,     presence: :true
-  validates :first_name_jp, presence: :true
-  validates :last_name_jp,  presence: :true
-  validates :birthday,      presence: :true
+  with_options presence: true do
+  validates :name
+  validates :first_name
+  validates :last_name
+  validates :first_name_jp
+  validates :last_name_jp
+  validates :birthday
+  end
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
