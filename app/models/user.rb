00bcd88841156213ca4cab31b7710309d873c37b
iamrubyist <takagioffcial4@gmail.com>
2021-02-28 16:53:18 +0900

    validateの記述
    what
    validateに記述
    what
    必要ないデータも受け取ってしまう

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name,          presence: :true
+  validates :first_name,    presence: :true
+  validates :last_name,     presence: :true
+  validates :first_name_jp, presence: :true
+  validates :last_name_jp,  presence: :true
+  validates :birthday,      presence: :true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
