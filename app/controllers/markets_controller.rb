a1ed9775efa83c2788963c72bbbd09483a5d998b
iamrubyist <takagioffcial4@gmail.com>
2021-02-28 14:40:03 +0900

    root to:の記載
    what
    ローカルホストに接続したときにhtme.erbが呼び出されるように記載した
    why
    yay!で止まってしまうため

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,3 +1,6 @@
 class MarketsController < ApplicationController
-  @markets = Market.all
+
+  def index
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: "markets#index"
   resources :markets, only: :index
 end
