47945a506b7dd9fa2fb680d2b8085e65935d7fc2
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 09:39:00 +0900

    正規表現の修正
    what
    正規表現の修正
    why
    「々」が弾かれてしまうため

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   with_options presence: true do
   validates :name
-  validates :first_name, with: /\A[ぁ-んァ-ヶ一-龥]/+\z
+  validates :first_name, with: /\A[ぁ-んァ-ン一-龥々]/+\z
   validates :last_name, with: /\A[ぁ-んァ-ヶ一-龥]/+\z
   validates :first_name_jp, with: /\A[ァ-ヶー－]+\z/
   validates :last_name_jp, with: /\A[ァ-ヶー－]+\z/
