d7c9c7dd0ca933a276f07be50ade838a6ac14dca
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 19:21:59 +0900

    marketsモデルにバリデーション記述
    what
    空では保存できなくした
    why
    からで保存されると困るから

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,16 @@
 class Market < ApplicationRecord
+  with_options presence: true do
+    validates :name 
+    validates :item_text
+    validates :category_id
+    validates :item_text
+    validates :delivery_id
+    validates :area_id
+    validates :days_to_ship_id
+    validates :price
+    validates :user
+  end 
+  
   has_one_attached :image
   belongs_to :user
 end
