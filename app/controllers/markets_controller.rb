1f992d3df3b01984a52cd204e5e661482bff4fe4
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 18:11:08 +0900

    ストロングパラメーター
    what
    ストロングパラメーターを設定
    why
    必要ないデータが送られてくるため

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -11,5 +11,8 @@ class MarketsController < ApplicationController
 
   def show
   end
-  
+ private
+
+ def market_params
+  params.require(:market).permit(:image,:name,:item_text,:category_id,:item_status,:delivery_id,:area_id,:days_to_ship_id,:price,:user).merge(user_id: current_user.id)
 end
