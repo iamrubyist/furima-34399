68b8eb9b0e172956a94720a501f89f645b1e137d
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 11:09:41 +0900

    半角に訂正
    what
    半角に訂正
    why
    エラーが出たため。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
    validates :password, length: { maximum: 6 }, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/+\z } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :first_name
     validates :last_name
   end
 
-   with_options presence:true, format: {with: /\A[ァ-ヶー－]+\z/}
+   with_options presence:true, format: {with: /\A[ァ-ヶー－]+\z/ } do
    validates :first_name_jp
    validates :last_name_jp
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ require 'rails_helper'
       expect(user.errors.full_messages).to include("Name can't be blank")
      end
      it "emailが空では登録できない" do
+      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      binding.pry
+      expect(user.errors.full_messages).to include("Email can't be blank")
      end
    end
  end
