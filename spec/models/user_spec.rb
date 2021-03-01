43f27e2b254d232e34b09591d5ffb2b45588f4e2
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 12:52:18 +0900

    記述してあるものの編集
    what
    コードをきれいにした
    why
    FactoryBotを使っているため

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
  describe "ユーザー新規登録" do
-      it 'nameが空だと登録できない' do
-        user = User.new(name: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
-        user.valid?
+  before do
+      @user = FactoryBot.build(:user)
+  end
+     it 'nameが空だと登録できない' do
+        @user.nickname = ''
+        @user.valid?
         expect(user.errors.full_messages).to include("Name can't be blank")
-      end
+     end
      it "emailが空では登録できない" do
-      user = User.new(name: "abe", email: "", password: "00000000", password_confirmation: "00000000")
-      user.valid?
+      @user.email = ''
+      @user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
      end
      it 'passwordが空だと登録できない' do
-      user = User.new(name: 'abe', email: 'kkk@gmail.com', password: '', password_confirmation: '00000000')
-      user.valid?
+      @user.password = ''
+      @user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
      end
    end
