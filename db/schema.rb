9af8d646d5730ed98c45258061ddb94b918e3749
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 17:58:25 +0900

    商品テーブルのカラム作成
    what
    商品テーブルのカラム作成
    why
    データを保存したいから

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_064015) do
+ActiveRecord::Schema.define(version: 2021_03_01_084506) do
+
+  create_table "markets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "item_text", null: false
+    t.integer "category_id", null: false
+    t.integer "item_status", null: false
+    t.integer "delivery_id", null: false
+    t.integer "area_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_markets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_02_28_064015) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "markets", "users"
 end
