7ab820a2449e52d6b2842834b006ad025bc77db3
iamrubyist <takagioffcial4@gmail.com>
2021-03-01 18:25:38 +0900

    バリデーションの設定
    what
    articleのバリデーションの設定
    why
    genreをgenereにしてしまった、スペルミス

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :genere
+
+  validates :title, :text, presence: true
+
+  validates :genere_id, numericality: { other_than: 1 } 
 end
