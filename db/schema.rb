facc0e9f2d54d2a11a0731226e346a43291846d9
iamrubyist <takagioffcial4@gmail.com>
2021-03-02 14:32:55 +0900

    カラム名の変更
    what
    integer型で保存したいので、_idをつける
    why
    わかりやすいように

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_084506) do
     t.string "name", null: false
     t.text "item_text", null: false
     t.integer "category_id", null: false
-    t.integer "item_status", null: false
+    t.integer "item_status_id", null: false
     t.integer "delivery_id", null: false
     t.integer "area_id", null: false
     t.integer "days_to_ship_id", null: false
