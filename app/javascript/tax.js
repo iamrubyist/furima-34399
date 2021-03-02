99c368364affa0cab484a70dcff68e79d302e5c5
iamrubyist <takagioffcial4@gmail.com>
2021-03-02 19:32:23 +0900

    JavaScript
    what
    価格の計算
    why
    よっしゃ！

---
+++ app/javascript/tax.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const taxInput = document.getElementById('add-tax-price');
+  const profitInput = document.getElementById('profit');
+  
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+  taxInput.innerHTML = Math.floor(inputValue*0.1)
+  profitInput.innerHTML = Math.floor(inputValue*0.9)
+})
+});
