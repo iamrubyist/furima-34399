cdc547b7369b696eb12a29f2da432939eb21e320
iamrubyist <takagioffcial4@gmail.com>
2021-03-02 20:43:03 +0900

    ルーティング

--- db/migrate/20210301084506_create_markets.rb
+++ db/migrate/20210301084506_create_markets.rb
@@ -4,7 +4,7 @@ class CreateMarkets < ActiveRecord::Migration[6.0]
       t.string     :name,            null: false
       t.text       :item_text,       null: false
       t.integer    :category_id,     null: false
-      t.integer    :item_status_id,     null: false
+      t.integer    :item_status_id,  null: false
       t.integer    :delivery_id,     null: false
       t.integer    :area_id,         null: false
       t.integer    :days_to_ship_id, null: false
