c2e07aa8a5623d61a12e4758a351c16979f2b63b
iamrubyist <takagioffcial4@gmail.com>
2021-03-02 12:31:33 +0900

    商品出品機能のコントローラー編集

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,12 +1,20 @@
 class MarketsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
+    @markets = Market.order("created_at DESC")
   end
   
   def new
+    @market = Market.new
   end
 
   def create
+    @market = Market.new(market_params)
+    if @market.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -14,6 +22,6 @@ class MarketsController < ApplicationController
  private
 
  def market_params
-  params.require(:market).permit(:image,:name,:item_status,:price,:user).merge(user_id: current_user.id)
+  params.require(:market).permit(:image,:name,:item_text,:category_id,:item_status_id,:delivery_id,:area_id,:days_to_ship_id,:price,:user).merge(user_id: current_user.id)
  end
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -3,7 +3,7 @@ class Market < ApplicationRecord
     validates :name 
     validates :item_text
     validates :category_id
-    validates :item_text
+    validates :item_status_id
     validates :delivery_id
     validates :area_id
     validates :days_to_ship_id
@@ -19,7 +19,7 @@ class Market < ApplicationRecord
   belongs_to :delivery
   belongs_to :area 
   belongs_to :days_to_ship
-  
+
   validates :title, :text, presence: true
 
   validates :genre_id, numericality: { other_than: 0 } 
