240b20a96b4f0b15ded815ac428b0c43952d83e1
iamrubyist <takagioffcial4@gmail.com>
2021-03-03 15:34:56 +0900

    with_optionでまとめる
    what
    with_optionでまとめる
    why
    コードをきれいにするため

--- app/models/market.rb
+++ app/models/market.rb
@@ -20,11 +20,13 @@ class Market < ApplicationRecord
   belongs_to :area 
   belongs_to :days_to_ship
 
-  validates :category_id,     numericality: { other_than: 0 } 
-  validates :item_status_id,  numericality: { other_than: 0 } 
-  validates :delivery_id,     numericality: { other_than: 0 } 
-  validates :area_id,         numericality: { other_than: 0 } 
-  validates :days_to_ship_id, numericality: { other_than: 0 }
+  with_options numericality: { other_than: 0 } do
+  validates :category_id 
+  validates :item_status_id
+  validates :delivery_id 
+  validates :area_id
+  validates :days_to_ship_id
+  end
   
   validates :price, format: { with: /\A[0-9]+\z/ }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 10_000_000 }
