72cebe72f75b2045838fee32983ee8c735f521d2
iamrubyist <takagioffcial4@gmail.com>
2021-03-03 14:53:58 +0900

    テストコードが緑色になるように変更

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Market, type: :model do
       @market = FactoryBot.build(:market)
     end
     it "全ての情報が正しく入力されており、商品出品ができること" do
-    expect(@user).to be_valid
+      expect(@market).to be_valid
     end
     it "商品出品が空では保存できない" do
       @market.name = ''
@@ -15,32 +15,32 @@ RSpec.describe Market, type: :model do
     it "商品の説明が空ではできない" do
       @market.item_text = ''
       @market.valid?
-      expect(@market.errors.full_messages).to include("Item_status can't be blank")
+      expect(@market.errors.full_messages).to include("Item text can't be blank", "User can't be blank", "User must exist")
     end
     it "カテゴリーが空では登録できない" do
       @market.category_id = ''
       @market.valid?
-      expect(@market.errors.full_messages).to include("Category_id can't be blank")
+      expect(@market.errors.full_messages).to include("Category can't be blank", "Category is not a number", "User can't be blank", "User must exist")
     end
     it '商品の状態が空では登録できない' do
       @market.item_status_id = ''
       @market.valid?
-      expect(@market.errors.full_messages).to include("Item_status_id can't be blank")
+      expect(@market.errors.full_messages).to include("Item status can't be blank", "User can't be blank", "User must exist")
     end
     it '配送料の負担が空では登録できない' do
       @market.delivery_id = ''
       @market.valid?
-      expect(@market.errors.full_messages).to include("Delivery_id can't be blank")
+      expect(@market.errors.full_messages).to include("Delivery can't be blank", "User can't be blank", "User must exist")
     end
     it '発送元の地域が空では登録できない' do
       @market.area_id = ''
       @market.valid?
-      expect(@market.errors.full_messages).to include("Area_id can't be blank")
+      expect(@market.errors.full_messages).to include("Area can't be blank", "User can't be blank", "User must exist")
     end
     it '発送までの日時が空では登録できない' do
       @market.days_to_ship_id = ''
       @market.valid?
-      expect(@market.errors.full_messages).to include("Days_to_ship_id can't be blank")
+      expect(@market.errors.full_messages).to include("Days to ship can't be blank", "User can't be blank", "User must exist")
     end
     it '販売価格が空では登録できない' do
       @market.price = ''
