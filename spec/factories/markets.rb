c99f43d48498d6124ca2d0eac274a1e940199457
iamrubyist <takagioffcial4@gmail.com>
2021-03-03 15:38:49 +0900

    FactoryBotの編集
    what
    文字型を数値型に変更
    why
    保存されるデータが数字型なので

--- spec/factories/markets.rb
+++ spec/factories/markets.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :market do
     name                  {'test'}
     item_text             {'これは限定商品です'}
-    category_id           {'1'}
-    item_status_id        {'1'}
-    delivery_id           {'1'}
-    area_id               {'1'}
-    days_to_ship_id       {'1'} 
-    price                 {'1000'}
+    category_id           {1}
+    item_status_id        {1}
+    delivery_id           {1}
+    area_id               {1}
+    days_to_ship_id       {1} 
+    price                 {1000}
   end
 end
