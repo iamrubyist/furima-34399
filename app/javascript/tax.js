c58b7ec8dacdedc853d6712f7713120d4ff1635a
iamrubyist <takagioffcial4@gmail.com>
2021-03-03 15:33:16 +0900

    JavaScriptの不要な記述削除
    what
    JavaScriptの不要な記述削除
    why
    コードをきれいにするため

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -5,7 +5,6 @@ window.addEventListener('load', () => {
   
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
   taxInput.innerHTML = Math.floor(inputValue*0.1)
   profitInput.innerHTML = Math.floor(inputValue*0.9)
 })
