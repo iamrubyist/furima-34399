a634af905afe0e458a4eb85eddbdd840b2aea6fb
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-04 16:57:36 +0900

    edit/updateコントローラーの記述
    what
    edit/updateコントローラーの記述
    why

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,5 +1,7 @@
 class MarketsController < ApplicationController
+  before_action :set_market, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @markets = Market.all.order(created_at: :desc)
   end
@@ -17,6 +19,16 @@ class MarketsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @market.update(market_params)
+      redirect_to prototype_path(@market)
+    else
+      render :edit
+    end
+  end
   def show
     @market = Market.find(params[:id])
   end
@@ -25,4 +37,13 @@ class MarketsController < ApplicationController
  def market_params
   params.require(:market).permit(:image,:name,:item_text,:category_id,:item_status_id,:delivery_id,:area_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
  end
+
+ def set_market
+  @market = Market.find(params[:id])
+ end
+
+ def move_to_index
+  redirect_to root_path unless current_user == @market.user
+end
+
 end
