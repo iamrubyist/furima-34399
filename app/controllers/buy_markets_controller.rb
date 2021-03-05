c2479630c5a288e20f8a726aa0fbfcef9c505c2c
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-05 12:47:35 +0900

    購入履歴コントローラーの記述
    what
    購入履歴コントローラーの記述
    why
    購入履歴の記述をして、購入機能コントローラーを削除した

--- app/controllers/buy_markets_controller.rb
+++ app/controllers/buy_markets_controller.rb
@@ -1,2 +1,24 @@
 class BuyMarketsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @buy_market = BuyMarket.create(buy_market_params)
+    Purchase.create(purchases_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def buy_market_params
+    params.permit().merge(user_id: current_user.id)
+  end
+
+  def purchases_params
+    params.permit(:postal_code_id,:prefectures,:municipality,:address,:phone_number,:buy_market).merge(user_id: current_user.id)
+  end
 end
