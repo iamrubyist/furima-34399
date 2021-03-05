8477f0e723b1e38b356860f6e8a9a926f753f0f2
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-05 12:13:28 +0900

    購入履歴モデル作成
    what
    購入履歴モデル作成
    why
    誰が何をかったのか知りたいから

---
+++ app/models/buy_market.rb
@@ -0,0 +1,2 @@
+class BuyMarket < ApplicationRecord
+end
---
+++ db/migrate/20210305030938_create_buy_markets.rb
@@ -0,0 +1,9 @@
+class CreateBuyMarkets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buy_markets do |t|
+      t.references :market null: false, foreign_key: true
+      t.references :user   null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
