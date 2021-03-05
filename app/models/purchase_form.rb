1b76311c368548f6f8db48be4d55b54d0a996ea4
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-05 11:15:46 +0900

    送られてくるデータの受取
    what
    formモデルに値が受け取られるように記述
    why
    どこに値を持っていけばいいかわからないため

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,12 +1,18 @@
 class PurchaseForm
   include ActiveModel::Model
 
-  attr_accessor :purchase, :address
+  attr_accessor :postal_code_id, :prefectures, :municipality, :address,:phone_number, :buy_market
   validates :purchase, presence: true
   validates :address, presence: true
  
   def save
-    Purchase.create(purchase: purchase)
+    Purchase.create(
+      postal_code_id: postal_code_id, 
+      prefectures: prefectures,
+      municipality: municipality,
+      address: address,
+      phone_number: phone_number,
+      buy_market: buy_market)
     Adderss.create(address: address)
   end
 end
