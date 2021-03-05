04c861b3c1228a801848cbe5302c9f0c6b2b0b75
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-05 17:07:21 +0900

    コントローラーの記述大幅変更
    what
    参考カリキュラムを真似して変更
    why
    変数の定義

--- app/controllers/buy_markets_controller.rb
+++ app/controllers/buy_markets_controller.rb
@@ -6,17 +6,16 @@ class BuyMarketsController < ApplicationController
   def new
   end
 
-  def create
-    @buy_market = BuyMarket.create(buy_market_params)
-    @purchase = Purchase.create(purchases_params)
-    redirect_to root_path
+  def create 
+    @purchase_form = PurchaseForm.new(purchases_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-
   private
-
-  def buy_market_params
-    params.permit().merge(user_id: current_user.id)
-  end
 
   def purchases_params
     params.permit(:postal_code_id,:prefectures,:municipality,:address,:phone_number,:buy_market).merge(user_id: current_user.id)
