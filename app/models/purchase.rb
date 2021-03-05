75f6bdcaef76849c40a73f10af4f2760ba019873
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-05 10:55:48 +0900

    商品購入テーブル作成
    what
    不要なモデル、テーブルの削除
    why
    正しく記述した。

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,10 @@
 class Purchase < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code_id
+    validates :prefectures
+    validates :municipality
+    validates :address
+    validates :phone_number
+    validates :buy_market
+  end
 end
--- db/migrate/20210305000837_create_purchases.rb
+++ db/migrate/20210305000837_create_purchases.rb
@@ -1,7 +1,13 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.string     :postal_code_id,     null: false
+      t.integer    :prefectures,        null: false
+      t.string     :municipality,       null: false
+      t.string     :address,            null: false
+      t.string     :phone_number,       null: false
+      t.string     :building_name,      null: false
+      t.references :buy_market,         null: false, foreign_key: true
       t.timestamps
     end
   end
