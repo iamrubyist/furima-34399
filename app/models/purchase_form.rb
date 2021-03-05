474ee2afcb616c7ed8e1fa600742da054e08f477
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-05 09:48:22 +0900

    formモデルの記述
    what
    見様見真似で記述
    why
    よくわかっていません

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,2 +1,12 @@
 class PurchaseForm
+  include ActiveModel::Model
+
+  attr_accessor :purchase, :address
+  validates :purchase, presence: true
+  validates :address, presence: true
+ 
+  def save
+    Purchase.create(purchase: purchase)
+    Adderss.create(address: address)
+  end
 end
