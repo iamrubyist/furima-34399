d55d182cf47a1f02ceb7010e76c7b68fddccc1a1
iamrubyist <78143516+TakagiMasahiro1@users.noreply.github.com>
2021-03-09 11:06:34 +0900

    決済機能のクライアントサイド実装
    what
    トークン情報をフォームに追加
    why

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -7,16 +7,26 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("purchase_form[number]"),
+      cvc: formData.get("purchase_form[cvc]"),
+      exp_month: formData.get("purchase_form[exp_month]"),
+      exp_year: `20${formData.get("purchase_form[exp_year]")}`,
     };
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+
+      document.getElementById("order_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
